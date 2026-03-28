974178c4e95dfff1dce48446cf20fa9813fbefdc
ParvChauhan97 <122476105+ParvChauhan97@users.noreply.github.com>
2026-03-28 22:41:27 +0530

    Create MiddleOfLinkedList.java

---
+++ LinkedList/MiddleOfLinkedList.java
@@ -0,0 +1,14 @@
+//This is Logic only because I'm Solved this question on leetcode.
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+       ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
