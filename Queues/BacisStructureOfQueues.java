fa6819118c36ebb5a012b6df7d5ce017dcdd39e2
ParvChauhan97 <122476105+ParvChauhan97@users.noreply.github.com>
2026-03-31 09:51:46 +0530

    Add basic queue structure implementation

---
+++ Queues/BacisStructureOfQueues.java
@@ -0,0 +1,19 @@
+package Queues;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BacisStructureOfQueues {
+    private static void display(Queue<Integer> q){
+        int n = q.size();
+        for(int i=1;i<=n;i++){
+            System.out.print(q.peek()+" ");
+            q.add(q.remove());
+        }
+        System.out.println();
+    }
+  
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(10); q.add(20); q.add(30); q.add(40); q.add(50);
+        display(q);
+    }
