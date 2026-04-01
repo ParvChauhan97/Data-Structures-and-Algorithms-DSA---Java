173efc3b8760eef266f30941c3b10f46905f2dda
ParvChauhan97 <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-01 10:43:44 +0530

    Add Reverse class to reverse a number recursively

---
+++ Recursion/Reversenumber.java
@@ -0,0 +1,13 @@
+package Recursion;
+
+public class Reverse {
+    public static int reverse(int n, int r){
+        if(n==0) return r;
+        return reverse(n/10,r*10+n%10);
+    }
+
+public static void main(String[] args) {
+        int n = 6587597;
+       System.out.println(reverse(n,0));
+    }
+}
