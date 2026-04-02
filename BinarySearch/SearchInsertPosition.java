68657e6e68961a3b1db2a78aef208ec689370dac
ParvChauhan97 <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-03 00:20:18 +0530

    Create SearchInsertPosition.java

---
+++ BinarySearch/SearchInsertPosition.java
@@ -0,0 +1,13 @@
+//This is Logic only because I'm Solved this question on leetcode.
+
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int lo = 0, hi = nums.length-1, idx = -1;
+        while(lo<=hi){
+            int mid = (lo+hi)/2;
+            if(nums[mid]>=target) hi = mid - 1;
+            else lo = mid + 1;
+        }
+        return lo;
+    }
+}
