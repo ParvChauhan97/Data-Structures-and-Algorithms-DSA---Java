12f0cc8393010028178081feca9f57cbeeb5908f
ParvChauhan97 <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-04 22:47:12 +0530

    Create invertTree.java

---
+++ BinaryTree/invertTree.java
@@ -0,0 +1,13 @@
+//This is Logic only because I'm Solved this question on leetcode.
+
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) return root;
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
+}
