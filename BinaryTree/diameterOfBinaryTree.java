ddbba43d382ce6395abd9423bfd44aed7904b0ff
ParvChauhan97 <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-04 07:17:48 +0530

    Create diameterOfBinaryTree.java

---
+++ BinaryTree/diameterOfBinaryTree.java
@@ -0,0 +1,18 @@
+//This is Logic only because I'm Solved this question on leetcode.
+
+class Solution {
+    int max = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        levels(root);
+        return max;
+    }
+
+    int levels(TreeNode root) {
+        if(root == null) return 0;
+        int leftLevels = levels(root.left);
+        int rightLevels = levels(root.right);
+        max = Math.max(max, leftLevels + rightLevels);
+        return 1 + Math.max(leftLevels, rightLevels);
+    }
+}
