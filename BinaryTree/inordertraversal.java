d2086313f6caabf4cb6ca6538c695b5102212ae1
ParvChauhan97 <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-04 07:24:09 +0530

    Create inordertraversal.java

---
+++ BinaryTree/inordertraversal.java
@@ -0,0 +1,15 @@
+//This is Logic only because I'm Solved this question on leetcode.
+
+class Solution {
+    public void inorder(TreeNode root, ArrayList<Integer> arr){
+        if(root == null) return;
+        inorder(root.left, arr);
+        arr.add(root.val);
+        inorder(root.right, arr);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        inorder(root, arr);
+        return arr;
+    }
+}
