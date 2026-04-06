a310e927d1736f412b7daff0453ba5798e6952ef
ParvChauhan97 <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-06 20:32:38 +0530

    Create flatten.java

---
+++ Binary Search Tree (BST)/flatten.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode curr = root;
+        while(curr!=null){
+            if(curr.left!=null){
+                TreeNode pred = curr.left;
+                while(pred.right!=null) pred = pred.right;
+                pred.right = curr.right;
+                curr.right = curr.left;
+                curr.left = null;
+                curr = curr.right;
+            }
+            else curr = curr.right;
+        }
+    }
+}
