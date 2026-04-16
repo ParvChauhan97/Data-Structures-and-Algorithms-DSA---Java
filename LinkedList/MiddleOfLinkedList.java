354fc77e84101652d9bcb528056b892f8c3f9d8b
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-04-17 00:59:41 +0530

    Update MiddleOfLinkedList.java

--- LinkedList/MiddleOfLinkedList.java
+++ LinkedList/MiddleOfLinkedList.java
@@ -4,7 +4,6 @@ class Solution {
     public ListNode middleNode(ListNode head) {
        ListNode slow = head;
         ListNode fast = head;
-
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
