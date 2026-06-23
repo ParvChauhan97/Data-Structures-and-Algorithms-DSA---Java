8530ca1781a5f278bd7a6a282dc902bc2f8ef2d4
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-06-23 10:19:01 +0530

    Update in searchMatrix method

--- BinarySearch/SearchInASortedMatrix.java
+++ BinarySearch/SearchInASortedMatrix.java
@@ -4,7 +4,8 @@ class Solution {
     public boolean searchMatrix(int[][] arr, int tar) {
         int rows = arr.length, cols = arr[0].length;
         int lo = 0, hi = rows*cols-1;
-        while(lo<=hi){
+        while(lo<=hi)
+        {
             int mid = (lo+hi)/2;
             int midRow = mid/cols, midCol = mid%cols;
             if(arr[midRow][midCol]==tar) return true;
