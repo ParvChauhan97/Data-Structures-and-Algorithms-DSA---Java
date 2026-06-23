09ab7ef32a1e30c48cdaac894087c914b3817429
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-06-23 10:19:33 +0530

    Update in search method

--- BinarySearch/SearchInRotatedSortedArray.java
+++ BinarySearch/SearchInRotatedSortedArray.java
@@ -3,7 +3,8 @@
 class Solution {
     public int search(int[] nums, int target) {
         int lo = 0, hi = nums.length - 1;
-        while(lo<=hi){
+        while(lo<=hi)
+        {
             int mid = lo + (hi-lo)/2;
             if(nums[mid]==target) return mid;
             else if(nums[lo]<=nums[mid]){ 
