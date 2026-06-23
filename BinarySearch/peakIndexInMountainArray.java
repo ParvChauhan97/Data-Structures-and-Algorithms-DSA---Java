ba664eeb10d6d69c60ee53917dff0bd21da4b21c
Parv Chauhan <122476105+ParvChauhan97@users.noreply.github.com>
2026-06-23 10:19:53 +0530

    Update in peakIndexInMountainArray

--- BinarySearch/peakIndexInMountainArray.java
+++ BinarySearch/peakIndexInMountainArray.java
@@ -3,7 +3,8 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
         int lo = 1, hi = arr.length - 2; 
-        while(lo<=hi){
+        while(lo<=hi)
+        {
             int mid = (lo+hi)/2;
             if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]) return mid;
             else if(arr[mid]>arr[mid-1] && arr[mid]<arr[mid+1]) lo = mid + 1;
